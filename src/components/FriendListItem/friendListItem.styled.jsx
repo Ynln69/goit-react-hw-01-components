50431c4d56cd040ff64b947b65c93fe335a1a603
Yana <yanalunina96@gmail.com>
2023-04-07 01:24:24 +0300

    done 3 task

---
+++ src/components/FriendListItem/friendListItem.styled.jsx
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+
+export const FriendsListItem = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  padding: 15px 150px 15px 10px;
+  box-shadow: 4px 4px 1px rgba(0, 0, 0, 0.3);
+  border-radius: 2px;
+  transition: all 0.3s ease;
+
+  background-color: #fff;
+  &:hover {
+    transform: scale(1.2);
+    background-color: #ffe4e1;
+  }
+`;
+
+export const ItemStatus = styled.span`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: ${props => {
+    return props.online ? 'green' : 'red';
+  }};
+`;
+
+export const AvatarItem = styled.img`
+  border-radius: 10%;
+  background-color: #afeeee;
+`;
+
+export const FriendName = styled.p`
+  font-size: 30px;
+  font-weight: 600;
+  color: #008080;
+  text-shadow: 1px 2px 2px rgba(0, 0, 0, 0.5);
+`;
